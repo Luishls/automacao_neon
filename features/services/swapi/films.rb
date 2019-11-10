ab1b700194c6447a60c844f24ad3ec9e807a7acc
7700354295 <luis.sousa-ext@viavarejo.com.br>
2019-11-10 15:46:42 -0300

    service films

---
+++ features/services/swapi/films.rb
@@ -0,0 +1,12 @@
+class Films
+  include HTTParty
+
+  headers 'Content-Type' => 'application/json'
+  base_uri 'https://swapi.co'
+
+  def get_films
+    response = self.class.get('api/films/')
+    generate_evidence(response)
+    response
+  end
+end
