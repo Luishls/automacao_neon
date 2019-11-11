01c34f5a8aaede7e8417ba7e68b7a534db382d3d
7700354295 <luis.sousa-ext@viavarejo.com.br>
2019-11-11 16:14:22 -0300

    service planets

---
+++ features/services/planets.rb
@@ -0,0 +1,20 @@
+class Planets
+  include HTTParty
+
+  headers 'Content-Type' => 'application/json'
+  base_uri 'https://swapi.co'
+
+  def get_planets
+    response = self.class.get('api/planets/')
+    generate_evidence(response)
+    response
+  end
+
+  def get_planets(count)
+    rand = Random.new
+    count = rand.Rand(1..10) + count
+    response = self.class.get("api/planets/#{count}")
+    generate_evidence(response)
+    response
+  end
+end
